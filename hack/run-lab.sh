04858370f7a8f0b3342f23ebe7a18e86a1c898df
Florian JUDITH <florian.judith.b@gmail.com>
2023-03-05 22:57:11 -0500

    Enabled cluster automatic initialization

--- hack/run-lab.sh
+++ hack/run-lab.sh
@@ -10,14 +10,16 @@ then
     cp .env.example .env
 fi
 
-
 set -o allexport
 source .env
 set +o allexport
 eok 'Environment variables initialized'
 
 docker compose up --detach --quiet-pull --remove-orphans
-eok "PostgreSQL and Jager started"
+eok "CockroachDB and Jager started"
+sleep 5
+docker compose exec roach1 sh -c 'cockroach sql --execute="SELECT VERSION()" --insecure || cockroach init --insecure'
+eok "Cockroachdb cluster \"http://localhost:8080\" initialized"
 
 
 python3 -m venv .venv/
@@ -26,8 +28,7 @@ python3 -m pip install --upgrade pip --quiet
 pip3 install -r requirements.txt --quiet
 eok 'Pythond virtual environment initialized'
 
-
 python3 tests/populate.py
-eok 'database populated using "tests/integration/fakenames.csv" dataset'
+eok 'Database populated using "tests/integration/fakenames.csv" dataset'
 
 eok 'Development environment successfully initialized'
